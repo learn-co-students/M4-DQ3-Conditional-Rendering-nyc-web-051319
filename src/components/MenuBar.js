b10e501e1c6167936d0b63f61f2a371ca98e0b57
Mendel Stillerman <menstiler@gmail.com>
2019-07-17 13:28:26 -0400

    Done.

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const MenuBar = (props) => {
 
+
   /*
 
-  The 'a' tags below are the menu items. Think about the way a menu 
+  The 'a' tags below are the menu items. Think about the way a menu
   should work. When you click a menu item, the button typically becomes
   'active' to indicate that it is currently selected. How could we achieve
   this programatically? What other behavior do we expect when we click
@@ -13,22 +14,23 @@ const MenuBar = (props) => {
 
   */
 
+
   return (
     <div className="ui four item menu">
-      <a className="item active" id="profile">
+      <a onClick={(e) => props.changeView(e)} className={props.currentState === 'profile' ? 'active item' : 'item' } id="profile">
         <i className="user large icon" id="profile"/>
       </a>
 
-      <a className="item" id="photo">
+      <a onClick={(e) => props.changeView(e)} className={props.currentState === 'photo' ? 'active item' : 'item' } id="photo">
         <i className="photo large icon" id="photo"/>
       </a>
 
-      <a className="item" id="cocktail">
+      <a onClick={(e) => props.changeView(e)} className={props.currentState === 'cocktail' ? 'active item' : 'item' } id="cocktail">
         <i className="cocktail large icon" id="cocktail"/>
       </a>
 
-      <a className="item" id="pokemon"> 
-        <i className=" themeisle large icon" id="pokemon"/>
+      <a onClick={(e) => props.changeView(e)} className={props.currentState === 'pokemon' ? 'active item' : 'item' } id="pokemon">
+        <i className="themeisle large icon" id="pokemon"/>
       </a>
     </div>
   )
--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -3,22 +3,43 @@ import MenuBar from '../components/MenuBar.js'
 import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
 
 class MainBox extends React.Component {
+  state = {
+    id: "profile"
+  }
 
+  changeView = (e) => {
+    this.setState({
+      id: e.target.id
+    })
+  }
+
+
+  detailsToDisplay = () => {
+    switch(this.state.id) {
+      case 'profile':
+        return <Profile />
+      case 'photo':
+        return <Photos />
+      case 'cocktail':
+        return <Cocktails />
+      case 'pokemon':
+        return <Pokemon />
+    }
+  }
 
   render() {
 
     /*
 
-    Replace the code below! Depending on what menu item is selected in the menu, I should render either a Profile, Photos, Cocktails, or Pokemon component.Think of a way to track which menu item was selected. Which component should have state? Which component should have methods to control state? Where should these methods be called?
+    Replace the code below! Depending on what menu item is selected in the menu, I should render either a Profile, Photos, Cocktails, or Pokemon component. Think of a way to track which menu item was selected. Which component should have state? Which component should have methods to control state? Where should these methods be called?
 
     */
 
-    const detailsToDisplay = <div>Hi, I'm a div!</div>
 
     return (
       <div>
-        <MenuBar />
-        {detailsToDisplay}
+        <MenuBar changeView={this.changeView} currentState={this.state.id} />
+        {this.detailsToDisplay()}
       </div>
     )
   }
