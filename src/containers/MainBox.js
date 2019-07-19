bc73d8215103d15835cbbb7a2244c834c61f939e
locky5 <daikevin19@yahoo.com>
2019-07-19 09:35:28 -0400

    Done.

--- src/containers/MainBox.js
+++ src/containers/MainBox.js
@@ -1,8 +1,56 @@
 import React from 'react'
 import MenuBar from '../components/MenuBar.js'
-import { Profile, Photos, Cocktails, Pokemon} from '../components/Pages.js'
+import { Profile, Photos, Cocktails, Pokemon } from '../components/Pages.js'
 
 class MainBox extends React.Component {
+
+  state = {
+    page: "Profile"
+  }
+
+  activateProfile = () => {
+    this.setState({
+      page: "Profile"
+    }, () => {
+      console.log(this.state)
+    })
+  }
+
+  activatePhotos = () => {
+    this.setState({
+      page: "Photos"
+    }, () => {
+      console.log(this.state)
+    })
+  }
+
+  activateCocktails = () => {
+    this.setState({
+      page: "Cocktails"
+    }, () => {
+      console.log(this.state)
+    })
+  }
+
+  activatePokemon = () => {
+    this.setState({
+      page: "Pokemon"
+    }, () => {
+      console.log(this.state)
+    })
+  }
+
+  choosePage = () => {
+    if (this.state.page === "Profile") {
+      return <Profile />
+    } else if (this.state.page === "Photos") {
+      return <Photos />
+    } else if (this.state.page === "Cocktails") {
+      return <Cocktails />
+    } else if (this.state.page === "Pokemon") {
+      return <Pokemon />
+    }
+  }
 
 
   render() {
@@ -17,8 +65,8 @@ class MainBox extends React.Component {
 
     return (
       <div>
-        <MenuBar />
-        {detailsToDisplay}
+        <MenuBar activateProfile={this.activateProfile} activatePhotos={this.activatePhotos} activateCocktails={this.activateCocktails} activatePokemon={this.activatePokemon} />
+        {this.choosePage()}
       </div>
     )
   }
